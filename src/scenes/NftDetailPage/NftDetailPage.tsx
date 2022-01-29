852ba87630df9ab67260dfcbb1d8ec2eb273708f
Connor Rothschild <connorrothschild@gmail.com>
2022-01-29 12:49:26 -0600

    improve styling on all screen sizes

--- src/scenes/NftDetailPage/NftDetailPage.tsx
+++ src/scenes/NftDetailPage/NftDetailPage.tsx
@@ -139,11 +139,6 @@ const StyledContentContainer = styled.div`
   }
 `;
 
-const StyledAssetAndNote = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
 const StyledNftDetailPage = styled(Page)`
   @media only screen and ${breakpoints.mobile} {
     margin-left: ${pageGutter.mobile}px;
